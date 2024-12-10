27c83cdd859c558a299dc66b910cf0ed30fc2b6e
Crystina <x978zhan@uwaterloo.ca>
2024-12-10 18:59:02 +0000

    inspect

---
+++ inspect/cluster_per_query.py
@@ -0,0 +1,46 @@
+"""
+assume it uses xor-tydi
+"""
+import os
+import json
+import argparse
+import datasets
+
+from collections import defaultdict
+
+cur_dir = os.path.dirname(os.path.abspath(__file__))
+
+def load_id2group():
+    file = "vocab.txt"
+    with open(os.path.join(cur_dir, file), "r") as f:
+        token2id = json.load(f)
+
+    id2group = defaultdict(list)
+    for token, id in token2id.items():
+        id2group[id].append(token)
+    return id2group
+
+
+def yield_query_tokens(query_file):
+    with open(query_file, "r") as f:
+        for line in f:
+            qid, tokens = line.strip().split("\t")
+            token_ids = tokens.split(" ")
+            yield qid, token_ids 
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--query_file", type=str, required=True)
+args = parser.parse_args()
+
+query_file = args.query_file
+dataset = datasets.load_dataset("crystina-z/xor-tydi", "targetQ")["dev"]
+qid_to_text = {qid: text for qid, text in zip(dataset["query_id"], dataset["query"])}
+id2group = load_id2group()
+
+
+for qid, token_ids in yield_query_tokens(query_file):
+    print(qid, qid_to_text[qid])
+    for token_id in token_ids:
+        print(id2group[token_id])
+    # break
+    import pdb ; pdb.set_trace()
